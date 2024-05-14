b7ce6fa8d93247905fb870b45dd3247c525fe60c
seedumar1999 <seedumar1999@gmail.com>
2024-05-14 09:45:48 -0700

    File Manager

---
+++ server.js
@@ -0,0 +1,14 @@
+
+import express from 'express';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
+
+const server = express();
+
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
+
+export default server;
+
